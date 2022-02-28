f7053af900f6ce0c678084691b1290f1c45c9eae
Simon Bein <simontheleg@gmail.com>
2022-02-28 12:39:11 +0100

    add poc for partial validation
    Signed-off-by: Simon Bein <simontheleg@gmail.com>

--- validationtest/validation_test.go
+++ validationtest/validation_test.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"testing"
 
+	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
 	ext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
@@ -52,7 +54,7 @@ func validatorFromCRD(crd io.Reader, desVer string) (*validate.SchemaValidator, 
 	return sv, nil
 }
 
-func TestValidation(t *testing.T) {
+func TestFullClusterValidation(t *testing.T) {
 
 	const crdpath = "cluster_crd.yaml"
 	tt := map[string]struct {
@@ -94,5 +96,47 @@ func TestValidation(t *testing.T) {
 			}
 
 		})
+	}
+}
+
+// For partial validation, we cannot make use of unstructured.Unstructured{} as
+// it has fields like 'Kind' set as mandatory. As a result how we use the
+// validator slightly differs. In a real case, we possibly have to use a const
+// version there
+func TestClusterSpecOnly(t *testing.T) {
+	const crdpath = "cluster_crd.yaml"
+	const crdversion = "v1"
+	const fieldname = "spec"
+	const fclusterspec = "illegal_cniplugin_spec_only.yaml"
+
+	f, err := os.Open(crdpath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	v, err := validatorFromCRD(f, crdversion)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// in order to make partial validation possible, we wrap our resource in the struct
+	// that corresponds to the full CRD and later filter out any responses that do not
+	// match the fieldname
+	cspec := &kubermaticv1.ClusterSpec{}
+	d, err := os.ReadFile(fclusterspec)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = yaml.Unmarshal(d, cspec)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cwrap := &kubermaticv1.Cluster{}
+	cwrap.Spec = *cspec
+
+	res := validation.ValidateCustomResource(nil, cwrap, v)
+	for _, e := range res {
+		if strings.HasPrefix(e.Field, fieldname) {
+			fmt.Println(e.Error())
+		}
 	}
 }
