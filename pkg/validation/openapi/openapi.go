c29d4f8eb11961a95c126f404f0c05e8153ce897
Simon Bein <simontheleg@gmail.com>
2022-03-14 17:00:05 +0100

    WIP refactor for v1beta1 CRDs. Tests currently failing
    Signed-off-by: Simon Bein <simontheleg@gmail.com>

--- pkg/validation/openapi/openapi.go
+++ pkg/validation/openapi/openapi.go
@@ -12,56 +12,96 @@ import (
 	"github.com/gobuffalo/flect"
 	ext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	extv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/kube-openapi/pkg/validation/validate"
 )
 
 // ValidatorFromCRD creates a new validator based on the supplied crd.
 // The CRD must be in yaml or json representation.
-// As a CRD can contain multiple versions, a desired one needs to be specified
+// As a v1 CRD can contain multiple versions, a desired one needs to be specified
+// When using beta1v1 CRDs, desVer can be left empty.
 func ValidatorFromCRD(crd io.Reader, desVer string) (*validate.SchemaValidator, error) {
-	crdv1 := &extv1.CustomResourceDefinition{}
 
+	u := &unstructured.Unstructured{}
 	dec := yaml.NewYAMLOrJSONDecoder(crd, 1024)
-	err := dec.Decode(crdv1)
-	if err != nil {
+	if err := dec.Decode(u); err != nil {
 		return nil, err
 	}
 
-	// Currently there is an issue in the k8s yaml package when working with documents that start with a blank line and the yaml document separator
+	// Currently there is an issue in the k8s yaml package when working with documents that start with a blank line followed by the yaml document separator
 	// (https://github.com/kubernetes/apimachinery/issues/133). So for now we work around the empty buffer by re-reading from it
 	// Note: alternative yaml packages (e.g. gopkg.in/yaml) cannot be used, as they do not handle kubernetes-specifc struct tags
-	if crdv1.TypeMeta.APIVersion == "" {
-		err := dec.Decode(crdv1)
+	if u.GetAPIVersion() == "" {
+		err := dec.Decode(u)
 		if err != nil {
 			return nil, err
 		}
 	}
 
+	var sv *validate.SchemaValidator
+	var err error
+	v := u.GetObjectKind().GroupVersionKind().Version
+	switch v {
+	case "v1beta1":
+		sv, err = v1beta1Validator(u)
+	case "v1":
+		if desVer == "" {
+			return nil, fmt.Errorf("when using v1 CRDs, desVer cannot be empty")
+		}
+		sv, err = v1Validator(u, desVer)
+	default:
+		return nil, fmt.Errorf("APIVersion %q of CRD is not supported", v)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return sv, nil
+}
+
+func v1beta1Validator(u *unstructured.Unstructured) (*validate.SchemaValidator, error) {
+	crdv1beta1 := &extv1beta1.CustomResourceDefinition{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.UnstructuredContent(), crdv1beta1); err != nil {
+		return nil, err
+	}
+	crdr := &ext.CustomResourceDefinition{}
+	if err := extv1beta1.Convert_v1beta1_CustomResourceDefinition_To_apiextensions_CustomResourceDefinition(crdv1beta1, crdr, nil); err != nil {
+		return nil, err
+	}
+	sv, _, err := validation.NewSchemaValidator(crdr.Spec.Validation)
+	if err != nil {
+		return nil, err
+	}
+
+	return sv, nil
+}
+
+func v1Validator(u *unstructured.Unstructured, desVer string) (*validate.SchemaValidator, error) {
+	crdv1 := &extv1.CustomResourceDefinition{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.UnstructuredContent(), crdv1); err != nil {
+		return nil, err
+	}
 	crdr := &ext.CustomResourceDefinition{}
 	if err := extv1.Convert_v1_CustomResourceDefinition_To_apiextensions_CustomResourceDefinition(crdv1, crdr, nil); err != nil {
 		return nil, err
 	}
 
+	var err error
 	var sv *validate.SchemaValidator
 	for _, ver := range crdr.Spec.Versions {
 		if ver.Name == desVer {
-
-			// if per-version validation is enabled, use it; otherwise use global validation
-			if ver.Schema != nil {
-				sv, _, err = validation.NewSchemaValidator(ver.Schema)
-			} else {
-				sv, _, err = validation.NewSchemaValidator(crdr.Spec.Validation)
-			}
+			sv, _, err = validation.NewSchemaValidator(ver.Schema)
 			if err != nil {
 				return nil, err
 			}
-			return sv, nil
+			break
 		}
 	}
-
 	if sv == nil {
 		return nil, fmt.Errorf("could not find SchemaValidator for desired version %q", desVer)
 	}
